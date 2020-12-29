a1444675049fef7a327ff9069fbad1560342fd4a
Dereck Wonnacott <dereck@gmail.com>
2020-12-29 12:30:21 -0500

    Initial Commit From Upstream
    v1.1.5.0 from VectroNav

---
+++ cpp/examples/getting_started/build/CMakeFiles/getting_started.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/getting_started.dir/main.cpp.o"
+  "getting_started"
+  "getting_started.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/getting_started.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cpp/examples/getting_started/build/libvncxx/CMakeFiles/libvncxx.dir/DependInfo.cmake
@@ -0,0 +1,41 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/dereck/Downloads/vnproglib-1-1-5/vnproglib-1.1.5.0/cpp/src/attitude.cpp" "/home/dereck/Downloads/vnproglib-1-1-5/vnproglib-1.1.5.0/cpp/examples/getting_started/build/libvncxx/CMakeFiles/libvncxx.dir/src/attitude.cpp.o"
+  "/home/dereck/Downloads/vnproglib-1-1-5/vnproglib-1.1.5.0/cpp/src/compositedata.cpp" "/home/dereck/Downloads/vnproglib-1-1-5/vnproglib-1.1.5.0/cpp/examples/getting_started/build/libvncxx/CMakeFiles/libvncxx.dir/src/compositedata.cpp.o"
+  "/home/dereck/Downloads/vnproglib-1-1-5/vnproglib-1.1.5.0/cpp/src/conversions.cpp" "/home/dereck/Downloads/vnproglib-1-1-5/vnproglib-1.1.5.0/cpp/examples/getting_started/build/libvncxx/CMakeFiles/libvncxx.dir/src/conversions.cpp.o"
+  "/home/dereck/Downloads/vnproglib-1-1-5/vnproglib-1.1.5.0/cpp/src/criticalsection.cpp" "/home/dereck/Downloads/vnproglib-1-1-5/vnproglib-1.1.5.0/cpp/examples/getting_started/build/libvncxx/CMakeFiles/libvncxx.dir/src/criticalsection.cpp.o"
+  "/home/dereck/Downloads/vnproglib-1-1-5/vnproglib-1.1.5.0/cpp/src/dllvalidator.cpp" "/home/dereck/Downloads/vnproglib-1-1-5/vnproglib-1.1.5.0/cpp/examples/getting_started/build/libvncxx/CMakeFiles/libvncxx.dir/src/dllvalidator.cpp.o"
+  "/home/dereck/Downloads/vnproglib-1-1-5/vnproglib-1.1.5.0/cpp/src/error_detection.cpp" "/home/dereck/Downloads/vnproglib-1-1-5/vnproglib-1.1.5.0/cpp/examples/getting_started/build/libvncxx/CMakeFiles/libvncxx.dir/src/error_detection.cpp.o"
+  "/home/dereck/Downloads/vnproglib-1-1-5/vnproglib-1.1.5.0/cpp/src/event.cpp" "/home/dereck/Downloads/vnproglib-1-1-5/vnproglib-1.1.5.0/cpp/examples/getting_started/build/libvncxx/CMakeFiles/libvncxx.dir/src/event.cpp.o"
+  "/home/dereck/Downloads/vnproglib-1-1-5/vnproglib-1.1.5.0/cpp/src/ezasyncdata.cpp" "/home/dereck/Downloads/vnproglib-1-1-5/vnproglib-1.1.5.0/cpp/examples/getting_started/build/libvncxx/CMakeFiles/libvncxx.dir/src/ezasyncdata.cpp.o"
+  "/home/dereck/Downloads/vnproglib-1-1-5/vnproglib-1.1.5.0/cpp/src/memoryport.cpp" "/home/dereck/Downloads/vnproglib-1-1-5/vnproglib-1.1.5.0/cpp/examples/getting_started/build/libvncxx/CMakeFiles/libvncxx.dir/src/memoryport.cpp.o"
+  "/home/dereck/Downloads/vnproglib-1-1-5/vnproglib-1.1.5.0/cpp/src/packet.cpp" "/home/dereck/Downloads/vnproglib-1-1-5/vnproglib-1.1.5.0/cpp/examples/getting_started/build/libvncxx/CMakeFiles/libvncxx.dir/src/packet.cpp.o"
+  "/home/dereck/Downloads/vnproglib-1-1-5/vnproglib-1.1.5.0/cpp/src/packetfinder.cpp" "/home/dereck/Downloads/vnproglib-1-1-5/vnproglib-1.1.5.0/cpp/examples/getting_started/build/libvncxx/CMakeFiles/libvncxx.dir/src/packetfinder.cpp.o"
+  "/home/dereck/Downloads/vnproglib-1-1-5/vnproglib-1.1.5.0/cpp/src/port.cpp" "/home/dereck/Downloads/vnproglib-1-1-5/vnproglib-1.1.5.0/cpp/examples/getting_started/build/libvncxx/CMakeFiles/libvncxx.dir/src/port.cpp.o"
+  "/home/dereck/Downloads/vnproglib-1-1-5/vnproglib-1.1.5.0/cpp/src/position.cpp" "/home/dereck/Downloads/vnproglib-1-1-5/vnproglib-1.1.5.0/cpp/examples/getting_started/build/libvncxx/CMakeFiles/libvncxx.dir/src/position.cpp.o"
+  "/home/dereck/Downloads/vnproglib-1-1-5/vnproglib-1.1.5.0/cpp/src/searcher.cpp" "/home/dereck/Downloads/vnproglib-1-1-5/vnproglib-1.1.5.0/cpp/examples/getting_started/build/libvncxx/CMakeFiles/libvncxx.dir/src/searcher.cpp.o"
+  "/home/dereck/Downloads/vnproglib-1-1-5/vnproglib-1.1.5.0/cpp/src/sensors.cpp" "/home/dereck/Downloads/vnproglib-1-1-5/vnproglib-1.1.5.0/cpp/examples/getting_started/build/libvncxx/CMakeFiles/libvncxx.dir/src/sensors.cpp.o"
+  "/home/dereck/Downloads/vnproglib-1-1-5/vnproglib-1.1.5.0/cpp/src/serialport.cpp" "/home/dereck/Downloads/vnproglib-1-1-5/vnproglib-1.1.5.0/cpp/examples/getting_started/build/libvncxx/CMakeFiles/libvncxx.dir/src/serialport.cpp.o"
+  "/home/dereck/Downloads/vnproglib-1-1-5/vnproglib-1.1.5.0/cpp/src/thread.cpp" "/home/dereck/Downloads/vnproglib-1-1-5/vnproglib-1.1.5.0/cpp/examples/getting_started/build/libvncxx/CMakeFiles/libvncxx.dir/src/thread.cpp.o"
+  "/home/dereck/Downloads/vnproglib-1-1-5/vnproglib-1.1.5.0/cpp/src/types.cpp" "/home/dereck/Downloads/vnproglib-1-1-5/vnproglib-1.1.5.0/cpp/examples/getting_started/build/libvncxx/CMakeFiles/libvncxx.dir/src/types.cpp.o"
+  "/home/dereck/Downloads/vnproglib-1-1-5/vnproglib-1.1.5.0/cpp/src/util.cpp" "/home/dereck/Downloads/vnproglib-1-1-5/vnproglib-1.1.5.0/cpp/examples/getting_started/build/libvncxx/CMakeFiles/libvncxx.dir/src/util.cpp.o"
+  "/home/dereck/Downloads/vnproglib-1-1-5/vnproglib-1.1.5.0/cpp/src/utilities.cpp" "/home/dereck/Downloads/vnproglib-1-1-5/vnproglib-1.1.5.0/cpp/examples/getting_started/build/libvncxx/CMakeFiles/libvncxx.dir/src/utilities.cpp.o"
+  "/home/dereck/Downloads/vnproglib-1-1-5/vnproglib-1.1.5.0/cpp/src/vntime.cpp" "/home/dereck/Downloads/vnproglib-1-1-5/vnproglib-1.1.5.0/cpp/examples/getting_started/build/libvncxx/CMakeFiles/libvncxx.dir/src/vntime.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../../../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
